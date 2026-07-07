25e9b0f029c21f4a25f7ec9a5decd2119a0a23ee
Prateek Sharma <prateek@Prateeks-MacBook-Pro.local>
2026-07-07 12:09:18 +0530

    style: increase padding between header and streak card even more

--- lib/features/tracking/presentation/tracking_screen.dart
+++ lib/features/tracking/presentation/tracking_screen.dart
@@ -526,7 +526,7 @@ class _TrackingScreenState extends ConsumerState<TrackingScreen> {
           ],
         ).animate().slideY(begin: -0.2).fadeIn(duration: 400.ms),
 
-        const SizedBox(height: 40),
+        const SizedBox(height: 64),
 
         GestureDetector(
           onTap: () {
